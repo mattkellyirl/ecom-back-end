56d0f1431b7faa4ed61f0c3180a180ab642c407c
mattkellyirl <matthew.kelly@digicom.net.au>
2024-03-22 16:55:38 +1030

    fixed typo in method

--- models/index.js
+++ models/index.js
@@ -3,24 +3,20 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 
-// Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 
-// Products belongToMany Tags (through ProductTag)
 Products.belongsToMany(Tag, {
   through: ProductTag,
 });
 
-// Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   foreignKey: ProductTag,
 });
 
